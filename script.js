dc2d4997ccf415e37becda2d4675f57e25a5a2e8
vikmeisterr <viktor.ivanovski11@gmail.com>
2023-11-23 20:27:51 +0100

    homework complete

--- script.js
+++ script.js
@@ -9,11 +9,31 @@ prevBtn.addEventListener("click", () => changeSlide(-1));
 
 function showSlide(index) {
   // recalculate the currentSlide
+  currentSlide = index % totalSlides;
+  console.log(currentSlide);
   // move the carousel to the currentSlide
+  switch (currentSlide) {
+    case 0:
+      carousel.style.transform = `translateX(0%)`;
+      break;
+    case 1:
+      carousel.style.transform = `translateX(-100%)`;
+      break;
+    case 2:
+      carousel.style.transform = `translateX(-200%)`;
+      break;
+    default:
+      currentSlide = 2;
+      carousel.style.transform = `translateX(-200%)`;
+      console.log(currentSlide);
+  }
 }
 
 function changeSlide(direction) {
   // get the currentSlide value and add the direction to it
+  currentSlide += direction;
   // assign that to a local variable called index
+  let index = currentSlide;
   // call showSlide with index
+  showSlide(index);
 }
